bbbc7a09f688a5d1b30f841be7a7aa1102ba74b5
Shwetank <mshwetank@gmail.com>
2025-11-26 15:12:47 +0530

    Begins

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,12 +1,23 @@
-console.log("Hello TS");
+// console.log("Hello TS");
 
-interface someValue {
-    name: string,
-    age : number
-}
+let newName:string = "Shwetank"
+newName = "ShwetankMishra"
+console.log(newName.toUpperCase())
 
-let someObj:someValue = {
-    name: "Shwetank",
-    age: 26
-}
-console.log(someObj)
+
+let amount:number =40;
+amount = 10-4;
+console.log(amount.toExponential(3));
+
+let isAwesome:boolean = true;
+isAwesome = false
+console.log(isAwesome);
+
+let newTestName:string = "Mishra";
+console.log(newTestName.charAt(3));
+
+
+const printNewName:boolean = false;
+
+console.log(`New Name is ${printNewName ? "Don" : "Nice1"}`)
+
